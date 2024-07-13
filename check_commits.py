6d7e5044a53357a02b89fe70b5bafbe2ea48a69e
safayavatsal <vatsalsafaya@gmail.com>
2024-07-13 11:32:16 +0530

    added python script

---
+++ check_commits.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import os
+
+GITHUB_API_URL = "https://api.github.com"
+REPO_OWNER = "safayavatsal"
+REPO_NAME = "simple_html_project"
+ACCESS_TOKEN = "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"
+
+def get_latest_commit_sha():
+    url = f"{GITHUB_API_URL}/repos/{REPO_OWNER}/{REPO_NAME}/commits"
+    headers = {
+        "Authorization": f"token {ACCESS_TOKEN}"
+    }
+    response = requests.get(url, headers=headers)
+    commits = response.json()
+    latest_commit_sha = commits[0]['sha']
+    return latest_commit_sha
+
+def main():
+    sha_file = 'latest_commit_sha.txt'
+    if os.path.exists(sha_file):
+        with open(sha_file, 'r') as file:
+            last_known_sha = file.read().strip()
+    else:
+        last_known_sha = ''
+
+    latest_commit_sha = get_latest_commit_sha()
+    
+    if last_known_sha != latest_commit_sha:
+        with open(sha_file, 'w') as file:
+            file.write(latest_commit_sha)
+        os.system('zsh deploy.sh')
+
+if __name__ == "__main__":
+    main()
